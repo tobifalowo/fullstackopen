fd896657416136dc3eaf9f9b1950abc8ce9f7a55
Tobi Falowo <tobifalowo@gmail.com>
2023-07-20 22:02:26 +0300

    Task 2.10 add key

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,6 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -80,7 +76,7 @@ const PersonForm = (props) => {
 
 const Persons = (props) => {
   return (
-    props.persons.map(person => <Person person={person} />)
+    props.persons.map(person => <Person person={person} key={person.name}/>)
   )
 }
 
