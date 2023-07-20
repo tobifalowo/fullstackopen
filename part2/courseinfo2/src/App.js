93f56f3a50512c11058c8152b04056fd7c55a388
Tobi Falowo <tobifalowo@gmail.com>
2023-07-20 20:36:50 +0300

    Part 2.5

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -1,3 +1,5 @@
+import { Course } from './Course.js'
+
 const App = () => {
   const courses = [
     {
@@ -48,48 +50,6 @@ const App = () => {
     <h1>Web Development Curriculum</h1>
     {courses.map(course => <Course course={course} />)}
   </>
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course.name} />
-      <Content parts={props.course.parts}/>
-      <Total parts={props.course.parts} />
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <h2>{props.course}</h2>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map(part => <Part part={part}/>)}
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </>
-  )
-}
-
-const Total = (props) => {
-  const nofExercises = props.parts.map(a => a.exercises).reduce((a, b) => {return a + b}, 0)
-
-  return(
-    <b>Total of {nofExercises} exercises</b>
-  )
 }
 
 export default App
---
+++ part2/courseinfo2/src/Course.js
@@ -0,0 +1,43 @@
+const Course = (props) => {
+    return (
+      <div>
+        <Header course={props.course.name} />
+        <Content parts={props.course.parts}/>
+        <Total parts={props.course.parts} />
+      </div>
+    )
+  }
+  
+  const Header = (props) => {
+    return (
+      <h2>{props.course}</h2>
+    )
+  }
+  
+  const Content = (props) => {
+    return (
+      <div>
+        {props.parts.map(part => <Part part={part}/>)}
+      </div>
+    )
+  }
+  
+  const Part = (props) => {
+    return (
+      <>
+        <p>
+          {props.part.name} {props.part.exercises}
+        </p>
+      </>
+    )
+  }
+  
+  const Total = (props) => {
+    const nofExercises = props.parts.map(a => a.exercises).reduce((a, b) => {return a + b}, 0)
+  
+    return(
+      <b>Total of {nofExercises} exercises</b>
+    )
+  }
+
+  export { Course }
