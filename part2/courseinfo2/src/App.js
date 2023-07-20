9262a8cea06530cf14d7af96868a5070aef105bd
Tobi Falowo <tobifalowo@gmail.com>
2023-07-20 20:22:37 +0300

    Part 2.1

---
+++ part2/courseinfo2/src/App.js
@@ -0,0 +1,71 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+
+
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts}/>
+      <Total parts={props.course.parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => <Part part={part}/>)}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  const nofExercises = props.parts.map(a => a.exercises).reduce((a, b) => {return a + b}, 0)
+
+  return(
+    <p>Number of exercises {nofExercises}</p>
+  )
+}
+
+export default App
