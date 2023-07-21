b9b26048011bc19f2081e9cf2636b021395be2e4
Tobi Falowo <tobifalowo@gmail.com>
2023-07-22 00:26:42 +0300

    Task 2.13

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -10,11 +11,10 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -29,13 +29,13 @@ const App = () => {
         name: newName,
         number: newNumber
       }
+      setNewName('')
+      setNewNumber('')
 
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
-          setNewName('')
-          setNewNumber('')
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
         })
     }
   }
