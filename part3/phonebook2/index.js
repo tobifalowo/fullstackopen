779009a97d0c16d761d5e0dd72165918edbeee0d
Tobi Falowo <tobifalowo@gmail.com>
2023-07-22 06:52:34 +0300

    Task 3.3

--- part3/phonebook2/index.js
+++ part3/phonebook2/index.js
@@ -28,17 +28,27 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
-  app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
-  
-  app.get('/info', (request, response) => {
-    const nofPeople = persons.length
-    const time = new Date()
-    response.send(`<p>Phonebook has info for ${nofPeople} people</p><p>${time}</p>`)
-  })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/info', (request, response) => {
+  const nofPeople = persons.length
+  const time = new Date()
+  response.send(`<p>Phonebook has info for ${nofPeople} people</p><p>${time}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
