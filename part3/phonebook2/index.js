e2dcb224d2fba40d0e597b3a16ce46b7beb34c0d
Tobi Falowo <tobifalowo@gmail.com>
2023-07-22 06:58:38 +0300

    Task 3.4

--- part3/phonebook2/index.js
+++ part3/phonebook2/index.js
@@ -42,6 +42,13 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
   const nofPeople = persons.length
   const time = new Date()
