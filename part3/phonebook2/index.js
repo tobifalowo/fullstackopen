66fc03f90ceca989c6d4c116687e04d6d420035d
Tobi Falowo <tobifalowo@gmail.com>
2023-07-22 07:31:59 +0300

    Task 3.6

--- part3/phonebook2/index.js
+++ part3/phonebook2/index.js
@@ -69,6 +69,11 @@ app.post('/api/persons', (request, response) => {
       error: 'number missing' 
     })
   }
+  if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
 
   const person = {
     name: body.name,
