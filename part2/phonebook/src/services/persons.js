1a17174a67a62841fcbc30b099a0cf4884eb69f0
Tobi Falowo <tobifalowo@gmail.com>
2023-07-26 21:27:52 +0300

    Task 3.11 baseUrl

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 // const baseUrl = 'http://localhost:3001/persons'
 // compatability with Part 3
-const baseUrl = 'http://localhost:3001/api/persons'
+// const baseUrl = 'http://localhost:3001/api/persons'
+// compatability with production build
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
