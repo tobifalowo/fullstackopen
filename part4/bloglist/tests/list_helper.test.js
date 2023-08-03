e06a1d06175a307dafaabcbd7ff725105dd7a726
Tobi Falowo <tobifalowo@gmail.com>
2023-08-03 05:59:35 +0300

    Task 4.6

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -120,3 +120,66 @@ describe('favorite blog', () => {
     expect(result).toEqual(listWithThreeBlogs[2])
   })
 })
+
+describe('most blogs', () => {
+  const emptyList = []
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Go To Statement Considered Harmful',
+      author: 'John Doe',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f6',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 13,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is null', () => {
+    const result = listHelper.mostBlogs(emptyList)
+    expect(result).toEqual(null)
+  })
+
+  test('when list has only one blog, equals that blog author with one blog', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    expect(result).toEqual({
+      author: listWithOneBlog[0].author,
+      blogs: 1
+    })
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostBlogs(listWithThreeBlogs)
+    expect(result).toEqual({
+      author: listWithOneBlog[0].author,
+      blogs: 2
+    })
+  })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+var lodash = require('lodash');
+
 const dummy = (blogs) => {
   return 1
 }
@@ -27,8 +29,31 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+// returns the author who has the largest amount of blogs
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  } else {
+    const grouped = Object.values(lodash.groupBy(blogs, blog => blog.author))
+    const top = grouped.reduce(
+      (a, c) => {
+        if (a.length > c.length) {
+          return a
+        } else {
+          return c
+        }
+      }
+    )
+    return {
+      author: top[0].author,
+      blogs: top.length
+    }
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
