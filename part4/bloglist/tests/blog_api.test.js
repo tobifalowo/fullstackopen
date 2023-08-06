0a820a14694d9d53e3695de312795d3f7fefbda4
Tobi Falowo <tobifalowo@gmail.com>
2023-08-06 11:11:42 +0300

    Task 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -27,6 +27,30 @@ test('the unique identifier property of the blog posts is named id', async () =>
   expect(exampleBlog.id).toBeDefined()
 })
 
+test('a valid blog post can be added', async () => {
+  const newBlog = {
+    title: 'Donuts Explained',
+    author: 'Homer Simpson',
+    url: 'http://www.blogger.xyz/homer/donuts',
+    likes: 0,
+  }
+  
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(titles).toContain(
+    'Donuts Explained'
+  )
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
